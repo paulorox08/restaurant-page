eac9616723cddaa98dae92e1b7e82b53eff9e2b2
Paulo Adeva <paulo.adeva@gmail.com>
2022-06-18 23:57:48 +1000

    added skeleton

--- src/index.js
+++ src/index.js
@@ -1,3 +1,72 @@
-console.log("Hello budy");
+import './style.css';
 
-alert("Hello");
+const content = document.querySelector('.content');
+
+function skeleton() {
+    
+    const header = document.createElement('div');
+    header.className = "header";
+    header.innerText = "Eggcellence"
+
+    content.appendChild(header);
+
+    const tabs = document.createElement('div');
+    tabs.className = "tabs";
+    
+    content.appendChild(tabs);
+
+    const about = document.createElement('div');
+    about.className = "about";
+    about.innerText = "About";
+
+    tabs.appendChild(about);
+
+    const recipes = document.createElement('div');
+    recipes.className = 'recipes';
+    recipes.innerText = "Recipes";
+
+    tabs.appendChild(recipes);
+
+    const contact = document.createElement('div');
+    contact.className = 'contact';
+    contact.innerText = "Contact Us";
+
+    tabs.appendChild(contact);
+
+};
+
+skeleton();
+
+function body() {
+
+    const aboutText = document.createElement('div');
+    aboutText.className = "aboutText";
+    aboutText.innerText = "This is the abouts page";
+
+    content.appendChild(aboutText);
+
+    const recipesText = document.createElement('div');
+    recipesText.className = "recipesText";
+    recipesText.innerText = "This is the recipes page";
+
+    content.appendChild(recipesText);
+
+    const contactText = document.createElement('div');
+    contactText.className = "contactText";
+    contactText.innerText = "This is the contacts page";
+
+    content.appendChild(contactText);
+}
+
+body();
+
+function showAbout() {
+    const about = document.querySelector('.about');
+    const aboutText = document.querySelector('.aboutText');
+
+    about.addEventListener('click', () => {
+        aboutText.style.visibility = "visible";
+    })
+}
+
+showAbout();
